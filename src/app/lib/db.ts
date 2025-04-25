8e112c05890ad8b1db46d483f5b4af67f20eaa0a
luaygitaris <luaygitaris.com>
2025-04-25 14:49:47 +0700

    caca

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,16 +1,11 @@
+// /lib/db.ts
 import mysql from 'mysql2/promise';
-import { config } from 'dotenv';
-
-config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: process.env.DB_HOST!,
+  user: process.env.DB_USER!,
+  password: process.env.DB_PASS!,
+  database: process.env.DB_NAME!,
 });
 
 export default pool;
